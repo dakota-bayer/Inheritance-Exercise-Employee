fdee4e9eaa480fe679d0a2c311c52526607a2a5c
dakota-bayer <dakota.bayer@gmail.com>
2022-03-26 11:44:57 -0500

    Testing methods that are available to subclasses

--- Inheritance Exercise - Employee/Program.cs
+++ Inheritance Exercise - Employee/Program.cs
@@ -18,7 +18,18 @@ namespace Inheritance_Exercise___Employee
             hourlyEmp3.AddTime(30);
             decimal hours = hourlyEmp3.HoursWorked();
 
-            //Left off at top of page 41
+            //emp1.AddTime(60);
+            //decimal h2 = emp1.HoursWorked();
+            //salEmp2 = AddTime(90);
+            //decimal h3 = salEmp2.HoursWorked();
+            //These doesn't work beacuse you cannot call methods declared in a object's subclasses or an object's sibling classes
+
+            //But you can call methods in super classes (if they are declared public)
+            string n1 = emp1.Name;
+            string n2 = salEmp2.Name;
+            string n3 = hourlyEmp3.Name;
+
+            
         }
     }
 }
